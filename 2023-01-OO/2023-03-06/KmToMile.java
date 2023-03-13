6cf8d70ae5ce96732ea005323a131bd90f8461f4
Gabriel Lopes <70214312+ghl0pes@users.noreply.github.com>
2023-03-13 20:11:20 -0300

    2023-03-13-Melhoria da classe que calcula km para milha

--- 2023-01-OO/2023-03-06/KmToMile.java
+++ 2023-01-OO/2023-03-06/KmToMile.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 public class KmToMile {
+    private double oneMile = 1.60934;
 
     public static void main (String[] args) {
+        KmToMile thisClass = new KmToMile();
+        
+        double kms = thisClass.calculateMile();
+        
+        System.out.println("Valor em milhas: " + kms);
+    }
+
+    public static double calculateMile () {
+        KmToMile thisClass = new KmToMile();
         Scanner sc = new Scanner(System.in);
-        double oneMile = 1.60934;
 
         System.out.println("Conversor de Kms para Milhas: \n");
         System.out.println("Digite um valor em Km para calcular:  ");
 
         double kms = sc.nextDouble();
-        double kmToMile = kms * oneMile;  
-
-        System.out.println("Valor em milhas: " + kmToMile);      
+        return kms * thisClass.oneMile;
     }
 }
